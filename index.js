fecb0e0d5dbfa3d914fce8df3470b6d11636c4c8
Blair Desjardins <blairmichaeldesjardins@gmail.com>
2019-11-05 20:30:31 -0500

    changed secret thingy to Puppet

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ server
 
 server.use(function(req, res, next) {
     if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-        jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+        jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'Puppet', function(err, decode) {
             if (err) req.user = undefined;
             req.user = decode;
             next();
