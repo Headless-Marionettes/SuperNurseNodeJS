a2a3da521c55047a1b1c79b04a24b74c7540a1ef
Ignat Pechkurenko <pechkig@gmail.com>
2019-11-16 14:21:30 -0500

    Added role checking in every endpoint

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var http = require('http');
 var mongoose = require("mongoose");
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
-var userHandlers = require('./userController.js');
 var errs = require('restify-errors');
 
 var port = process.env.PORT;
@@ -53,8 +52,8 @@ var userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.comparePassword = function(password) {
-  console.log("compare password called");
-  return bcrypt.compareSync(password, this.hash_password);
+    console.log("compare password called");
+    return bcrypt.compareSync(password, this.hash_password);
 };
 
 // Compiles the schema into a model, opening (or creating, if
@@ -101,12 +100,12 @@ server
 
 
 server.use(function(req, res, next) {
-  console.log("auth middleware")
+    console.log("auth middleware")
     if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
         jwt.verify(req.headers.authorization.split(' ')[1], 'Puppet', function(err, decode) {
             console.log("JWT was received")
             if (err) {
-                console.log("There was an error in the middleware")
+                console.log("Error verifying JWT" + err)
                 req.user = undefined;
                 next();
             }
@@ -119,11 +118,10 @@ server.use(function(req, res, next) {
     }
 });
 
+var adminLevel = "Admin"
+var nurseLevel = "Nurse"
 
-// Get all patients in the system
-server.get('/patients', function(req, res, next) {
-    console.log('GET request: patients');
-    
+function CheckUserRole(req, res, next, level) {
     if (req.user) {
         var id = req.user._id
         User.findOne({ _id: id }).exec(function(error, user) {
@@ -131,159 +129,202 @@ server.get('/patients', function(req, res, next) {
             if (error) {
                 console.log("find user error")
                 return next(new errs.UnauthorizedError('Unauthorized user!'))
-            }
-            else if (user) {
-                if(user.role != "Admin") {
-                    console.log("user role not admin")
-                    return next(new errs.UnauthorizedError('Unauthorized user!'))
-                }
-                else
-                {
-                    console.log('before next')
-                    next();
+            } else if (user) {
+                if (level === nurseLevel) {
+                    if (user.role != "Admin" && user.role != "Nurse") {
+                        console.log("User role not nurse or admin")
+                        return next(new errs.UnauthorizedError('Unauthorized user (not Nurse or Admin)!'))
+                    } else {
+                        console.log('before next')
+                        next();
+                    }
+                } else if (level === adminLevel) {
+                    if (user.role != "Admin") {
+                        console.log("user role not admin")
+                        return next(new errs.UnauthorizedError('Unauthorized user (not Admin)!'))
+                    } else {
+                        console.log('before next')
+                        next();
+                    }
                 }
             }
         })
     } else {
         return next(new errs.UnauthorizedError('Unauthorized user!'))
     }
-}, function (req, res, next) {
-    console.log(` req is ${req}`)
-    console.log(` res is ${res}`)
+}
 
-    // Find every entity within the given collection
-    Patient.find({}).exec(function(error, result) {
-        console.log("trying to find patients")
-        if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
-        res.send(result);
-    });
-})
+// Get all patients in the system
+server.get('/patients',
+    function(req, res, next) {
+        console.log('GET request: patients');
+        CheckUserRole(req, res, next, nurseLevel);
+    },
+    function(req, res, next) {
+        console.log(` req is ${req}`)
+        console.log(` res is ${res}`)
+
+        // Find every entity within the given collection
+        Patient.find({}).exec(function(error, result) {
+            console.log("trying to find patients")
+            if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
+            res.send(result);
+        });
+    })
 
 
 // Get a single patient by their patient id
-server.get('/patients/:id', function(req, res, next) {
-    console.log('GET request: patients/' + req.params.id);
+server.get('/patients/:id',
+    function(req, res, next) {
+        console.log('GET request: patients/' + req.params.id);
+        CheckUserRole(req, res, next, nurseLevel);
+    },
+    function(req, res, next) {
+        // Find a single patient by their id
+        Patient.find({ _id: req.params.id }).exec(function(error, patient) {
+            // If there are any errors, pass them to next in the correct format
+            //if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    // Find a single patient by their id
-    Patient.find({ _id: req.params.id }).exec(function(error, patient) {
-        // If there are any errors, pass them to next in the correct format
-        //if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-        if (patient) {
-            // Send the patient if no issues
-            res.send(patient)
-        } else {
-            // Send 404 header if the patient doesn't exist
-            res.send(404)
-        }
+            if (patient) {
+                // Send the patient if no issues
+                res.send(patient)
+            } else {
+                // Send 404 header if the patient doesn't exist
+                res.send(404)
+            }
+        })
     })
-})
 
 
 // Create a new patient
-server.post('/patients', function(req, res, next) {
-    console.log('POST request: patients');
-    // Make sure name is defined
-    if (req.body.first_name === undefined) {
-        // If there are any errors, pass them to next in the correct format
-        return next(new errs.InvalidArgumentError('first_name must be supplied'))
-    }
-    if (req.body.last_name === undefined) {
-        // If there are any errors, pass them to next in the correct format
-        return next(new errs.InvalidArgumentError('last_name must be supplied'))
-    }
+server.post('/patients',
+    function(req, res, next) {
+        console.log('POST request: patients');
+        CheckUserRole(req, res, next, adminLevel);
+    },
+    function(req, res, next) {
 
-    // Creating new patient.
-    var newPatient = new Patient({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        height: req.body.height,
-        weight: req.body.weight,
-        date_of_birth: req.body.date_of_birth,
-        floor: req.body.floor
-    });
+        // Make sure name is defined
+        if (req.body.first_name === undefined) {
+            // If there are any errors, pass them to next in the correct format
+            return next(new errs.InvalidArgumentError('first_name must be supplied'))
+        }
+        if (req.body.last_name === undefined) {
+            // If there are any errors, pass them to next in the correct format
+            return next(new errs.InvalidArgumentError('last_name must be supplied'))
+        }
+
+        // Creating new patient.
+        var newPatient = new Patient({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            height: req.body.height,
+            weight: req.body.weight,
+            date_of_birth: req.body.date_of_birth,
+            floor: req.body.floor
+        });
 
 
-    // Create the patient and saving to db
-    newPatient.save(function(error, result) {
+        // Create the patient and saving to db
+        newPatient.save(function(error, result) {
 
-        // If there are any errors, pass them to next in the correct format
-        if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
+            // If there are any errors, pass them to next in the correct format
+            if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
 
-        // Send the patient if no issues
-        res.send(201, result)
+            // Send the patient if no issues
+            res.send(201, result)
+        })
     })
-})
 
 // Delete all patients
-server.del('/patients', function(req, res, next) {
-    console.log('DEL request: patients/' + req.params.id);
-    Patient.remove({}, function(error, result) {
-        // If there are any errors, pass them to next in the correct format
-        if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
+server.del('/patients',
+    function(req, res, next) {
+        console.log('DEL request: patients/' + req.params.id);
+        CheckUserRole(req, res, next, adminLevel);
+    },
+    function(req, res, next) {
+        console.log('DEL request: patients/' + req.params.id);
+        Patient.remove({}, function(error, result) {
+            // If there are any errors, pass them to next in the correct format
+            if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
 
-        // Send a 200 OK response
-        res.send()
-    });
-})
+            // Send a 200 OK response
+            res.send()
+        });
+    })
 
 // Delete patient with the given id
-server.del('/patients/:id', function(req, res, next) {
-    console.log('DEL request: patients');
-    Patient.remove({ _id: req.params.id }, function(error, result) {
-        // If there are any errors, pass them to next in the correct format
-        if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
+server.del('/patients/:id',
+    function(req, res, next) {
+        console.log('DEL request: patients/' + req.params.id);
+        CheckUserRole(req, res, next, adminLevel);
+    },
+    function(req, res, next) {
+        Patient.remove({ _id: req.params.id }, function(error, result) {
+            // If there are any errors, pass them to next in the correct format
+            if (error) return next(new errs.InvalidArgumentError(JSON.stringify(error.errors)))
 
-        // Send a 200 OK response
-        res.send()
-    });
-})
+            // Send a 200 OK response
+            res.send()
+        });
+    })
 
 // Get all records from a single patient by their patient id
-server.get('/patients/:id/records', function(req, res, next) {
-    console.log('GET request: patients/' + req.params.id + '/records');
+server.get('/patients/:id/records',
+    function(req, res, next) {
+        console.log('GET request: patients/' + req.params.id + '/records');
+        CheckUserRole(req, res, next, nurseLevel);
+    },
+    function(req, res, next) {
+        // Find a single patient by their id
+        Patient.findOne({ _id: req.params.id }).exec(function(error, patient) {
+            // If there are any errors, pass them to next in the correct format
+            //if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-    // Find a single patient by their id
-    Patient.findOne({ _id: req.params.id }).exec(function(error, patient) {
-        // If there are any errors, pass them to next in the correct format
-        //if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-        if (patient) {
-            // Send the patient if no issues
-            res.send(patient.records)
-        } else {
-            // Send 404 header if the patient doesn't exist
-            res.send(404)
-        }
-    })
-})
-
-server.post('/patients/:id/records', function(req, res, next) {
-    console.log('POST request: patients/' + req.params.id + '/records')
-
-    Patient.findOneAndUpdate({ _id: req.params.id }).exec(function(error, patient) {
-        if (patient) {
-            patient.records.push({
-                date: req.body.date,
-                blood_pressure: req.body.blood_pressure,
-                respiratory_rate: req.body.respiratory_rate,
-                blood_oxygen_level: req.body.blood_oxygen_level,
-                heart_beat_rate: req.heart_beat_rate
-            })
-            patient.save(function(err) {
-                if (err) {
-                    console.log(err)
-                    return
-                }
-            })
-            res.send(patient)
-        } else {
-            // Send 404 header if the patient doesn't exist
-            res.send(404)
-        }
+            if (patient) {
+                // Send the patient if no issues
+                res.send(patient.records)
+            } else {
+                // Send 404 header if the patient doesn't exist
+                res.send(404)
+            }
+        })
     })
 
-})
+// Add new record for the patinet with specific id
+server.post('/patients/:id/records',
+    function(req, res, next) {
+        console.log('POST request: patients/' + req.params.id + '/records')
+        CheckUserRole(req, res, next, adminLevel);
+    },
+    function(req, res, next) {
+        Patient.findOneAndUpdate({ _id: req.params.id }).exec(function(error, patient) {
+            if (patient) {
+                patient.records.push({
+                    date: req.body.date,
+                    blood_pressure: req.body.blood_pressure,
+                    respiratory_rate: req.body.respiratory_rate,
+                    blood_oxygen_level: req.body.blood_oxygen_level,
+                    heart_beat_rate: req.heart_beat_rate
+                })
+                patient.save(function(err) {
+                    if (err) {
+                        console.log(err)
+                        return
+                    }
+                })
+                res.send(patient)
+            } else {
+                // Send 404 header if the patient doesn't exist
+                res.send(404)
+            }
+        })
+
+    })
+
+
+
+// Site admin section
 
 // Get all users in the system
 server.get('/users', function(req, res, next) {
@@ -295,18 +336,25 @@ server.get('/users', function(req, res, next) {
     });
 })
 
-// Create a new patient
+// Create a new user
 server.post('/users', function(req, res, next) {
     console.log('POST request: user');
 
     // Make sure name is defined
     if (req.body.email === undefined) {
         // If there are any errors, pass them to next in the correct format
-        return next(new errs.InvalidArgumentError('email must be supplied'))
+        return next(new errs.InvalidArgumentError('Email of new user must be supplied'))
     }
     if (req.body.password === undefined) {
         // If there are any errors, pass them to next in the correct format
-        return next(new errs.InvalidArgumentError('password must be supplied'))
+        return next(new errs.InvalidArgumentError('Password of new user must be supplied'))
+    }
+    if (req.body.role === undefined) {
+        return next(new errs.InvalidArgumentError('Role of new user must be supplied'))
+    }
+    console.log("New user role is " + req.body.role)
+    if (req.body.role != "Admin" && req.body.role != "Nurse") {
+        return next(new errs.InvalidArgumentError('Undefined role for new user'))
     }
 
     // Creating new user.
@@ -328,25 +376,25 @@ server.post('/users', function(req, res, next) {
     })
 })
 
-  server.post('/auth/signin', function(req, res, next) {
+// Sign in endpoint, email and password are required
+server.post('/auth/signin', function(req, res, next) {
     console.log('POST request: auth/signin');
     User.findOne({
-      email: req.body.email
+        email: req.body.email
     }, function(err, user) {
-      if (err) throw err;
-      if (!user) {
-        return next(new errs.UnauthorizedError('Authentication failed. User not found.'))
-      } else if (user) {
-        if (!user.comparePassword(req.body.password)) {
-          return next(new errs.UnauthorizedError('Authentication failed. Wrong password.'))
-        } else {
-            var login = user.email;
-            var role = user.role;
-            var userJwt = jwt.sign({ email: user.email, _id: user._id}, 'Puppet', {
-                expiresIn: '1h'});
-                
-          res.send(201, {token: userJwt});
+        if (err) throw err;
+        if (!user) {
+            return next(new errs.UnauthorizedError('Authentication failed. User not found.'))
+        } else if (user) {
+            if (!user.comparePassword(req.body.password)) {
+                return next(new errs.UnauthorizedError('Authentication failed. Wrong password.'))
+            } else {
+                var userJwt = jwt.sign({ email: user.email, _id: user._id }, 'Puppet', {
+                    expiresIn: '1h'
+                });
+
+                res.send(201, { token: userJwt });
+            }
         }
-      }
     });
-  })
+})
